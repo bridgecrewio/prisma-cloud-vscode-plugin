5b0d63ad4bca867793dcfd20700b6c640d7b22ab
PavloYakovenko <pyakovenko@paloaltonetworks.com>
2023-07-11 14:23:52 +0300

    [BCE-18764] initial primary sidebar functionality

--- src/extension.ts
+++ src/extension.ts
@@ -3,11 +3,13 @@ import * as vscode from 'vscode';
 import { registerCommands } from './commands';
 import { registerEvents } from './events';
 import { initializeServices } from './services';
+import { registerSidebar } from './views/interface/primarySidebar/views';
 
 export function activate(context: vscode.ExtensionContext) {
 	registerCommands(context);
 	registerEvents();
 	initializeServices(context);
+	registerSidebar();
 }
 
 export function deactivate() {}
---
+++ src/views/interface/primarySidebar/services/treeDataProvidersContainer.ts
@@ -0,0 +1,26 @@
+import { TreeDataProvider } from "../dataProviders/abstractTreeDataProvider";
+import { IaCTreeDataProvider } from "../dataProviders/iacTreeDataProvider";
+import { SecretsTreeDataProvider } from "../dataProviders/secretsTreeDataProvider";
+import { VulnerabilitiesTreeDataProvider } from "../dataProviders/vulnerabilitiesTreeDataProvider";
+import { LicensesTreeDataProvider } from "../dataProviders/licensesTreeDataProvider";
+
+export class TreeDataProvidersContainer {
+    public static iacTreeDataProvider: TreeDataProvider;
+    public static secretsTreeDataProvicer: TreeDataProvider;
+    public static vulnerabilitiesTreeDataProvider: TreeDataProvider;
+    public static licensesTreeDataProvider: TreeDataProvider;
+
+    static {
+        TreeDataProvidersContainer.iacTreeDataProvider = new IaCTreeDataProvider();
+        TreeDataProvidersContainer.secretsTreeDataProvicer = new SecretsTreeDataProvider();
+        TreeDataProvidersContainer.vulnerabilitiesTreeDataProvider = new VulnerabilitiesTreeDataProvider();
+        TreeDataProvidersContainer.licensesTreeDataProvider = new LicensesTreeDataProvider();
+    }
+
+    public static refresh() {
+        TreeDataProvidersContainer.iacTreeDataProvider.refresh();
+        TreeDataProvidersContainer.secretsTreeDataProvicer.refresh();
+        TreeDataProvidersContainer.vulnerabilitiesTreeDataProvider.refresh();
+        TreeDataProvidersContainer.licensesTreeDataProvider.refresh();
+    }
+}
