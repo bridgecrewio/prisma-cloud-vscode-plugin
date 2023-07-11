5fddd93e678374026dd6a60c784012cd9006c495
PavloYakovenko <pyakovenko@paloaltonetworks.com>
2023-07-11 13:57:23 +0300

    Revert "[BCE-18764] added tree data functionality for primary sidebar"
    This reverts commit eaecb0ff35b170c680aae2b6a1d8a81a799ac166.

--- src/extension.ts
+++ src/extension.ts
@@ -2,12 +2,10 @@ import * as vscode from 'vscode';
 
 import { registerCommands } from './commands';
 import { registerEvents } from './events';
-import { registerSidebar } from './views/interface/primarySidebar/views';
 
 export function activate(context: vscode.ExtensionContext) {
 	registerCommands(context);
 	registerEvents();
-	registerSidebar();
 }
 
 export function deactivate() {}
