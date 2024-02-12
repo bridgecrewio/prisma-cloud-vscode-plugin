b40f9c4bd23b6e36434c7fc9ae7d8cd7918bda52
PavloYakovenko <pyakovenko@paloaltonetworks.com>
2024-02-12 10:16:37 +0200

    fixed show settings query string

--- src/commands/checkov/showSettings.ts
+++ src/commands/checkov/showSettings.ts
@@ -2,6 +2,6 @@ import * as vscode from 'vscode';
 
 export class ShowSettings {
     static execute() {
-        vscode.commands.executeCommand('workbench.action.openSettings', '@ext:prisma-cloud.prisma-cloud-vscode-plugin');
+        vscode.commands.executeCommand('workbench.action.openSettings', '@ext:"palo alto networks - prisma cloud.prisma-cloud"');
     }
 }
