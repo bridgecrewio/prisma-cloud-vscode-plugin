3d4a2b45732fa2cfc76ed0cf07bfaf45449a85a4
PavloYakovenko <pyakovenko@paloaltonetworks.com>
2024-02-15 12:33:58 +0200

    changed extensionId for marketplace

--- src/commands/checkov/showSettings.ts
+++ src/commands/checkov/showSettings.ts
@@ -2,6 +2,6 @@ import * as vscode from 'vscode';
 
 export class ShowSettings {
     static execute() {
-        vscode.commands.executeCommand('workbench.action.openSettings', '@ext:"palo alto networks - prisma cloud.prisma-cloud"');
+        vscode.commands.executeCommand('workbench.action.openSettings', '@ext:"prismacloud.prisma-cloud"');
     }
 }
