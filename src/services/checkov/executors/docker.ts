ff9b5c8375e86385429ea21205319472dc21c3c7
PavloYakovenko <pyakovenko@paloaltonetworks.com>
2024-02-16 12:29:16 +0200

    improved logs for debuging and added error log for docker when container fails

--- src/services/checkov/executors/docker.ts
+++ src/services/checkov/executors/docker.ts
@@ -28,7 +28,7 @@ export class DockerExecutor extends AbstractExecutor {
             ...DockerExecutor.getCheckovCliParams(installation, files),
         ];
 
-        console.log(`${installation.entrypoint} ${args.join(' ')}`);
+        console.log(`${installation.entrypoint} ${args.join(' ').replace(/[^:\s]*::[^:\s]*/, '')}`);
         DockerExecutor.activeProcess = spawn(installation.entrypoint, args, { shell: true });
         const executionResult = await DockerExecutor.handleProcessOutput(DockerExecutor.activeProcess);
         AbstractExecutor.isScanInProgress = false;
